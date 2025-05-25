e1940ba0513180eac22aff4fc9e756afdb5b1e62
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-05-25 12:50:36 +0600

    first commit

---
+++ Assignment_PostgreSQL.sql
@@ -0,0 +1,68 @@
+-- Active: 1747456666201@@localhost@5432@ph
+CREATE DATABASE conservation_db;
+
+
+CREATE TABLE rangers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
+
+-- DROP TABLE rangers;
+
+-- DROP TABLE species;
+
+-- DROP TABLE sightings;
+
+
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(150) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT REFERENCES rangers(id) NOT NULL,
+    species_id INT REFERENCES species(species_id) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    notes TEXT
+);
+
+INSERT INTO rangers (name, region) VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
+
+
+
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+SELECT * FROM rangers;
+SELECT * FROM species;  
+SELECT * FROM sightings;
+
+-- problem 01
+INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
+
+-- problem 02 Count unique species ever sighted.
+SELECT COUNT(*) AS unique_species_count FROM (
+    SELECT species_id
+    FROM sightings
+    GROUP BY species_id
+) AS unique_species;
